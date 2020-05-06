621283cff96837630c9121600c84302b5e587d49
wehmam <readytosurff@gmail.com>
2020-05-06 12:35:27 +0700

    struktur project pt garuda

---
+++ database/migrations/2020_05_06_052059_create_karyawans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKaryawansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawans', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('nik')->unique();
+            $table->string('nama');
+            $table->string('alamat');
+            $table->bigInteger('no_hp');
+            $table->integer('umur');
+            $table->char('jenis_kelamin',1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawans');
+    }
+}
